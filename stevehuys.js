e87a7d5b8e61775ec18fe1de3dbe15dc935e696b
Andrew0Andrew <software.engineer.andrew@gmail.com>
2023-07-06 19:25:29 +0400

    changed response

--- stevehuys.js
+++ stevehuys.js
@@ -2,9 +2,8 @@ const http = require('http');
 const port = 3000;
 const server = http.createServer((req,res) => {
     res.writeHead(200);
-    res.end('hello word');
+    res.end('Hello word!');
 });
 
 server.listen(port);
 console.log(`server with port ${port} is on`)
-
